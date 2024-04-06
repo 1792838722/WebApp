86fc3464a53a9e3e883369766a3f3833d8b7c381
FenriX <1097797606@qq.com>
2024-04-06 23:27:51 +0800

    修复强制刷新静态文件缓存

--- chart/settings.py
+++ chart/settings.py
@@ -125,15 +125,15 @@ MEDIA_URL = '/resource/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'resource')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_ROOT = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

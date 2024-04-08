0667dbd01a9d74d344a76530cb68c0eca6fb868c
byte_remote <stuchfun@outlook.com>
2024-04-08 15:48:03 +0800

    initial commit

--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,12 @@
 import os.path
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from .b64_to_img import b64_to_img
 from .models import *
 from django.views import View
-from .exercise import test_out
-from .exercise import Show_result
-
+import base64, requests, json
 
 # Create your views here.
 
@@ -23,13 +25,14 @@ class Predict(View):
 
         upload_file_name = upload_file.name
         patient_name = upload_file_name[:upload_file_name.find('.')]
-        file_folder_path = os.path.join('resource', 'patient', get_time(), patient_name)
-        if os.path.exists(os.path.join(file_folder_path, patient_name)):
+        upload_time = get_time()
+        folder_path = os.path.join('resource', 'patient', upload_time, patient_name)
+        if os.path.exists(os.path.join(folder_path, patient_name)):
             value = {'msg': '上传文件重复'}
             return render(request, 'app1/predict.html', context=value)  # 文件已存在，跳过
 
-        if not os.path.exists(file_folder_path):
-            os.makedirs(file_folder_path)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
         # 在 /resource 中不重复地创建文件夹 /patient /{{ time }} /{{ patient_name }}
 
         # upload_image_name = upload_image_name.replace('.',
@@ -41,15 +44,39 @@ class Predict(View):
         # 重复文件添加随机后缀
 
         patient = Medical.objects.create(name=patient_name, raw_file=upload_file)
-        test_out.generate_mha(file_folder_path)
-        Show_result.generate_img(folder_path=file_folder_path, patient_name=patient_name)
+
+        # test_out.generate_mha(file_folder_path)
+        # Show_result.generate_img(folder_path=file_folder_path, patient_name=patient_name)
         # 文件处理及重命名
 
-        patient.pre_img.name = os.path.join('patient', get_time(), patient_name, patient_name + '_pre.png')
-        patient.tar_img.name = os.path.join('patient', get_time(), patient_name, patient_name + '_tar.png')
-        patient.save()
-        value = {'patient': patient}
-        return render(request, 'app1/predict.html', context=value)
+        raw_file = upload_file.read()
+        encoded_data = base64.b64encode(raw_file).decode('utf-8')
+        data_dict = {
+            'raw_file': encoded_data,
+            'time': upload_time,
+            'patient_name': patient_name,
+        }
+        data_json = json.dumps(data_dict)
+        url = 'http://10.106.13.36:12500/predict'
+        response = requests.post(url, json=data_json, headers={'Content-Type': 'application/json'})
+
+        if response.status_code == 200:
+            print('OK!')
+            data_return = response.json()
+            pre_img_data = base64.b64decode(data_return['pre_img'])
+            tar_img_data = base64.b64decode(data_return['tar_img'])
+            b64_to_img(os.path.join(folder_path, patient_name + '_pre.png'), pre_img_data)
+            b64_to_img(os.path.join(folder_path, patient_name + '_tar.png'), tar_img_data)
+            patient.pre_img.name = os.path.join(folder_path, patient_name + '_pre.png')
+            patient.tar_img.name = os.path.join(folder_path, patient_name + '_tar.png')
+            patient.save()
+            value = {'patient': patient}
+            return render(request, 'app1/predict.html', context=value)
+        elif response.status_code == 408:
+            print(408)
+            return HttpResponse(status=503)
+        else:
+            return HttpResponse(status=500)
 
 
 class Intro(View):
